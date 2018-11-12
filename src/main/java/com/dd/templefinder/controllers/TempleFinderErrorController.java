5cab2a00424135d8d3f87512e6b8302e5b70dd8c
adi <adithya.vadlamudi@gmail.com>
2018-11-12 16:54:51 +0100

    404 error handled with error controller and error jsp

---
+++ src/main/java/com/dd/templefinder/controllers/TempleFinderErrorController.java
@@ -0,0 +1,20 @@
+package com.dd.templefinder.controllers;
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class TempleFinderErrorController implements ErrorController  {
+ 
+    @RequestMapping("/error")
+    public String handleError() {
+        //do something like logging
+        return "error";
+    }
+ 
+    @Override
+    public String getErrorPath() {
+        return "/error";
+    }
+}
---
+++ src/main/webapp/WEB-INF/views/error.jsp
@@ -0,0 +1,19 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<title>Temple Finder App</title>
+<style type="text/css">
+.errorBody {
+    text-align:center;
+}
+</style>
+</head>
+<body class="errorBody">
+	<h1>Something went wrong!</h1>
+	<h2>Our Engineers are on it</h2>
+	<a href="/">Go Home</a>
+</body>
+</html>
